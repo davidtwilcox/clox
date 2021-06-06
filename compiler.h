5ef610bd6fcaa71a84ec5c3864be3598666d3693
David T Wilcox <david@dtwil.co>
2021-06-06 04:52:31 -0500

    Parser complete for simple math
    Parser complete for simple math

--- compiler.h
+++ compiler.h
@@ -1,6 +1,8 @@
 #ifndef CLOX_COMPILER_H
 #define CLOX_COMPILER_H
 
-void compile(const char* source);
+#include "vm.h"
+
+bool compile(const char* source, Chunk* chunk);
 
 #endif // CLOX_COMPILER_H
