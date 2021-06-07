04ae726b3af4302ca35d8e5d93b947baff7e7b03
David T Wilcox <david@dtwil.co>
2021-06-07 08:54:04 -0500

    Strings and string equality / concatenation now functional. Eliminated object heap memory leak
    Strings and string equality / concatenation now functional. Eliminated object heap memory leak

--- compiler.h
+++ compiler.h
@@ -1,6 +1,7 @@
 #ifndef CLOX_COMPILER_H
 #define CLOX_COMPILER_H
 
+#include "object.h"
 #include "vm.h"
 
 bool compile(const char* source, Chunk* chunk);
--- memory.c
+++ memory.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 
 #include "memory.h"
+#include "vm.h"
 
 // Reallocates memory for code chunks. Operations determined by oldSize / newSize
 // parameter values:
@@ -19,4 +20,24 @@ void* reallocate(void* pointer, size_t oldSize, size_t newSize) {
   void* result = realloc(pointer, newSize);
   if (result == NULL) exit(1);
   return result;
+}
+
+static void freeObject(Obj* object) {
+  switch (object->type) {
+  case OBJ_STRING: {
+    ObjString* string = (ObjString*)object;
+    FREE_ARRAY(char, string->chars, string->length + 1);
+    FREE(ObjString, object);
+    break;
+  }
+  }
+}
+
+void freeObjects() {
+  Obj* object = vm.objects;
+  while (object != NULL) {
+    Obj* next = object->next;
+    freeObject(object);
+    object = next;
+  }
 }
