b78fa2ff73707d4752a41dc530e723712b0404c2
David T Wilcox <david@dtwil.co>
2021-06-12 03:58:57 -0500

    for loops implemented
    for loops implemented

--- debug.c
+++ debug.c
@@ -94,7 +94,7 @@ int disassembleInstruction(Chunk* chunk, int offset) {
   case OP_JUMP_IF_FALSE:
     return jumpInstruction("OP_JUMP_IF_FALSE", 1, chunk, offset);
   case OP_LOOP:
-    return jumpInstruction("OP_LOOP", -1, chunk, offset); 
+    return jumpInstruction("OP_LOOP", -1, chunk, offset);
   case OP_RETURN:
     return simpleInstruction("OP_RETURN", offset);
   default:
