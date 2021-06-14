394b0cf1b10b43fea7f59a7968e460880271878f
David T Wilcox <david@dtwil.co>
2021-06-14 04:18:19 -0500

    Functions and function calls working
    Functions and function calls working

--- object.h
+++ object.h
@@ -2,16 +2,20 @@
 #define CLOX_OBJECT_H
 
 #include "common.h"
+#include "chunk.h"
 #include "value.h"
 
 #define OBJ_TYPE(value)     (AS_OBJ(value)->type)
 
+#define IS_FUNCTION(value)  isObjType(value, OBJ_FUNCTION)
 #define IS_STRING(value)    isObjType(value, OBJ_STRING)
 
+#define AS_FUNCTION(value)  ((ObjFunction*)AS_OBJ(value))
 #define AS_STRING(value)    ((ObjString*)AS_OBJ(value))
 #define AS_CSTRING(value)   (((ObjString*)AS_OBJ(value))->chars)
 
 typedef enum {
+  OBJ_FUNCTION,
   OBJ_STRING,
 } ObjType;
 
@@ -20,12 +24,21 @@ struct Obj {
   struct Obj* next;
 };
 
+typedef struct {
+  Obj obj;
+  int arity;
+  Chunk chunk;
+  ObjString* name;
+} ObjFunction;
+
 struct ObjString {
   Obj obj;
   int length;
   char* chars;
   uint32_t hash;
 };
+
+ObjFunction* newFunction();
 
 ObjString* takeString(char* chars, int length);
 ObjString* copyString(const char* chars, int length);
